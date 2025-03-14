d59d400fd93e2323f4678c229843e03326e7c48f
LuckMonkeys <34481971+LuckMonkeys@users.noreply.github.com>
2025-03-14 20:11:42 +0800

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -7,7 +7,7 @@
 
 from .process_dataset import process_sft_dataset, get_dataset, process_dpo_dataset
 from .template import get_formatting_prompts_func, TEMPLATE_DICT
-from .utils import cosine_learning_rate, insert_false_knowledge
-from .log import logger
+from .utils import cosine_learning_rate, insert_false_knowledge, load_model_from_ckpt, flatten_dict, flatten_model, flatten_tensors, insert_false_knowledge_backup, cal_dist, test_defense, replace_k_percent, replace_k_percent_upd, param_proj, init_plot
+from .log import logger, PrintLogger
 from .state_dict import get_model_state, set_model_state
 from .const import LLaMA_ALL_TARGET_MODULES, LLaMA_TARGET_MODULES
